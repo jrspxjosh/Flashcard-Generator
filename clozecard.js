33e04c54d97bf0174386a8a1af77707b5f88a603
Joshua Serrano-Patron <jrsp@Joshuas-MBP.home>
2017-06-29 01:03:46 -0400

    commit

--- clozecard.js
+++ clozecard.js
@@ -1,14 +1,19 @@
-// var firstPresidentCloze = new ClozeCard(
-//     "George Washington was the first president of the United States.", "George Washington");
-// // "George Washington"
+var ClozeCard = function(text, cloze){
+  this.cloze = cloze;
+  this.fullText = text;
+  this.partial  =  function() {
+    if(text.includes (cloze)){
+      return text.replace(cloze,"...");
+    }
+    else {
+      return "error";
+    }
+  }
+}
+var firstPresidentCloze = new ClozeCard( "George Washington was the first president of the United States.", "George Washington");
 // console.log(firstPresidentCloze.cloze);
-// // " ... was the first president of the United States.
-// console.log(firstPresidentCloze.partial); "
-// // "George Washington was the first president of the United States.
-// console.log(firstPresidentCloze.fullText): "
-// // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
-// var brokenCloze = new ClozeCard("This doesn't work", "oops");
-
+// console.log(firstPresidentCloze.fullText);
+console.log(firstPresidentCloze.partial());
 
 
 var ClozeCard = function(text, cloze){
@@ -23,7 +28,7 @@ var ClozeCard = function(text, cloze){
     }
   }
 }
-var firstPresidentCloze = new ClozeCard( "George Washington was the first president of the United States.", "green eggs and ham");
+var loopsCloze = new ClozeCard( "What are the two different types of loops.", "For && While");
 // console.log(firstPresidentCloze.cloze);
 // console.log(firstPresidentCloze.fullText);
-console.log(firstPresidentCloze.partial());
+console.log(loopsCloze.partial());
